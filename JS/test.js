72f0d5dfa8fd8aff6a31a9ace0200d6654b30919
Norimk <153037@csgaugustinus.nl>
2021-11-25 11:08:59 +0000

    h

--- JS/test.js
+++ JS/test.js
@@ -1,14 +1,25 @@
 function setup() {
   canvas = createCanvas(450,450);
-  background('silver');
+  background('pink');
   canvas.parent('processing');
   //noLoop();
 }
+function tekenKerk(kleur,kleurRondje){
 
+  fill(kleur);
+  rect(300,175,100,300);
+  fill('DarkSeaGreen');
+  triangle(300,175,400,175,350,75);
+  fill(kleurRondje);
+  ellipse(350,250,50,50);
+}
 function draw() {
   noStroke();
-  fill('steelblue');
-  ellipse(0,0,800);
-  fill('deepskyblue');
-  ellipse(450,450,400);
+  tekenKerk('lightgreen', 'white');
+
+  translate(-150,0);
+  tekenKerk('white', 'lightgreen');
+
+  translate(-150,0);
+  tekenKerk('lightgreen', 'white');
 }
