38e7326134819fa8bff4b9cd371c60954bf35ab0
Norimk <153037@csgaugustinus.nl>
2022-01-24 10:18:41 +0000

    gj

--- JS/H1/H1O01.js
+++ JS/H1/H1O01.js
@@ -1,18 +1,23 @@
 function setup() {
-  canvas = createCanvas(450,450);
-  background('grey');
+  canvas = createCanvas(500,500);
+  background('lightblue');
   canvas.parent('processing');
   noLoop();
 }
 
 function draw() {
-  noStroke();
-  fill('green');
-  ellipse(225,225,400);
-  fill('white');
-  ellipse(225,225,300);
-  stroke('green');
-  fill('white');
-  strokeWeight(10);
-  rect(125,125,200,200);  
+  //maan
+  fill('lightyellow');
+  ellipse(50,50,60);
+ for(var n= 1;n <= 4;n ++) {
+   tekenKerk();
+   translate(110,0);
+ }
+}
+
+function tekenKerk() {
+  fill('pink');
+  rect(200,250,100,300);
+  fill('red');
+  triangle(200,250,300,250,250,150);
 }
